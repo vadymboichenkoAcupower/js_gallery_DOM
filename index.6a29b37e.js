5bc2d0a26b740112f5e32f66a4b4d6b25474b4de
vadymboichenkoAcupower <vadym.boichenko@acupower.co.uk>
2024-04-11 19:50:33 +0300

    make build

---
+++ index.6a29b37e.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("thumbs"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".gallery__thumb");if(r){var a=r.closest("a");a&&(t.src=a.href)}});
+//# sourceMappingURL=index.6a29b37e.js.map
